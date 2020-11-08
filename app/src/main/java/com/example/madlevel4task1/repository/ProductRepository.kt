9feb421c7dd9008dfa4bf542431631ce6064942c
Skander <skander@bibani.nl>
2020-11-08 14:14:58 +0100

    Add ProductRepository

---
+++ app/src/main/java/com/example/madlevel4task1/repository/ProductRepository.kt
@@ -0,0 +1,32 @@
+package com.example.madlevel4task1.repository
+
+import android.content.Context
+import com.example.madlevel4task1.dao.ProductDao
+import com.example.madlevel4task1.database.ShoppingListRoomDatabase
+import com.example.madlevel4task1.model.Product
+
+class ProductRepository(context: Context) {
+
+    private val productDao: ProductDao
+
+    init {
+        val shoppingListRoomDatabase = ShoppingListRoomDatabase.getDatabase(context)
+        productDao = shoppingListRoomDatabase!!.productDao()
+    }
+
+    suspend fun getAllProducts(): List<Product> {
+        return productDao.getAllProducts()
+    }
+
+    suspend fun insertProduct(product: Product) {
+        productDao.insertProduct(product)
+    }
+
+    suspend fun deleteProduct(product: Product) {
+        productDao.deleteProduct(product)
+    }
+
+    suspend fun deleteAllProducts() {
+        productDao.deleteAllProducts()
+    }
+}
