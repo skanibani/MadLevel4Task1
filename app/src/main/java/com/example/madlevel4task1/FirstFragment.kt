1f1ebf89f7a1f6a85437a6e4e580b4035ca11c03
Skander <skander@bibani.nl>
2020-11-08 15:59:36 +0100

    Fragment gets fetches data from database

--- app/src/main/java/com/example/madlevel4task1/FirstFragment.kt
+++ app/src/main/java/com/example/madlevel4task1/FirstFragment.kt
@@ -7,13 +7,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel4task1.model.Product
 import com.example.madlevel4task1.repository.ProductRepository
+import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_first.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    // Room
     private lateinit var productRepository: ProductRepository
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
+    // Recyclerview
+    private var products: ArrayList<Product> = arrayListOf<Product>()
+    private val productAdapter = ProductAdapter(products)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +42,28 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         // Repository needs context
         productRepository = ProductRepository(requireContext())
+
+        initViews()
+
+    }
+
+    private fun initViews() {
+        rvProducts.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rvProducts.adapter = productAdapter
+        rvProducts.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+    }
+
+    private fun getShoppingListFromDatabase() {
+        mainScope.launch {
+            // Moves data handling from the main (UI) thread to the IO thread
+            val products = withContext(Dispatchers.IO) {
+                productRepository.getAllProducts()
+            }
+            this@FirstFragment.products.clear()
+            this@FirstFragment.products.addAll(products)
+            this@FirstFragment.productAdapter.notifyDataSetChanged()
+        }
     }
 }
