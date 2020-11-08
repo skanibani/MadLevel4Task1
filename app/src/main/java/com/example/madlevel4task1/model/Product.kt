9192f6c77f504aa71b4633ec6db4e41946293f53
Skander <skander@bibani.nl>
2020-11-08 13:47:22 +0100

    Add Product model

---
+++ app/src/main/java/com/example/madlevel4task1/model/Product.kt
@@ -0,0 +1,18 @@
+package com.example.madlevel4task1.model
+
+import androidx.room.Entity
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "productTable")
+data class Product(
+        @ColumnInfo(name = "name")
+        var productName: String,
+
+        @ColumnInfo(name = "quantity")
+        var productQuantity: Short,
+
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "id")
+        var id: Long?
+)
